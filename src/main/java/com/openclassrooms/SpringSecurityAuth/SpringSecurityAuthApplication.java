0db65968368b6b8e3b52762181b125301e90b739
l_flo <l_flo@10.97.2.53>
2024-10-24 11:28:53 +0200

    Config spring security

--- src/main/java/com/openclassrooms/SpringSecurityAuth/SpringSecurityAuthApplication.java
+++ src/main/java/com/openclassrooms/SpringSecurityAuth/SpringSecurityAuthApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityAuthApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringSecurityAuthApplication.class, args);
+		
 	}
 
 }
